3d41b6597bb057a09a4cc3a811f543d1510e8a65
Jim Fingal <jim.fingal@gmail.com>
2014-12-31 10:59:36 -0800

    image tests, and update get access token script

--- get_access_token.py
+++ get_access_token.py
@@ -1,11 +1,7 @@
-
 import tweepy
 import os
+import sys
 
-app_name = "TINY"
-
-consumer_key = os.environ.get(app_name + '_CONSUMER_KEY')
-consumer_secret = os.environ.get(app_name + '_CONSUMER_SECRET')
 
 def oauth_login(consumer_key, consumer_secret):
     """Authenticate with twitter using OAuth"""
@@ -19,6 +15,25 @@ def oauth_login(consumer_key, consumer_secret):
     return auth
  
 if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
+        print "Usage: python get_access_token.py [APPNAME]"
+        sys.exit(0)
+
+    app_name = sys.argv[1]
+
+    consumer_key = os.environ.get(app_name + '_CONSUMER_KEY')
+    consumer_secret = os.environ.get(app_name + '_CONSUMER_SECRET')
+
+    if not consumer_key or not consumer_secret:
+        print "Error: Environment variables not set"
+        print "Please re-run with these in your environment:"
+        print "   " + app_name + '_CONSUMER_KEY'
+        print "   " + app_name + '_CONSUMER_SECRET'
+
+        sys.exit(0) 
+
+
     auth = oauth_login(consumer_key, consumer_secret)
     print "Access Token ::  %s" % auth.access_token
     print "Access Token Secret ::  %s" %  auth.access_token_secret
